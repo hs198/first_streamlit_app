6b8f574e97597891364c85a56b85ac6722240b1f
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-18 15:36:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard- boiled Free-Range Egg')
 streamlit.text('🥑Avacado Toast')
 
+
+
+Import Pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+streamlit.dataframe(my_fruit_list
