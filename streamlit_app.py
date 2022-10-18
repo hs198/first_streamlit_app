9c0df287a8a97a609a66f4590af328631c95f7b4
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-18 15:44:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text('🥑Avacado Toast')
 
 
 Import Pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
-streamlit.dataframe(my_fruit_list
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
