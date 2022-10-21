1ffec6c21e31015313414934e2d694f72f80e6aa
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-20 21:10:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
 #New Section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
