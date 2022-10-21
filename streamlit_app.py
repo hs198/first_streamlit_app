0a7a50cec6bf442e3f8e192bd546fe8dc44bbf52
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-21 16:05:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list ")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
   
 # Allow the end user to add a fruit to the list
 #import pandas
