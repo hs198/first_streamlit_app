2f28a09d9c6518ff381f60beeaa08843bdf989b7
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-24 16:36:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,19 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
                                          
       #except URLError as e:
       #streamlit.error()
-                                          
+                                      
+# New section to dispaly  fruitvice api response
+streamlit.header('Fruityvice Fruit Advise!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+     streamlit.error("Please select a fruit to get information.")
+  else:
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+      
+except URLError as e:
+    streamlit.error()
 
 
 
