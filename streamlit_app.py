2ef6a46c89b6e754989f0771b89e4b33d60ead2a
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-24 09:27:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ streamlit.dataframe(my_data_rows)
 
 import pandas
 add_my_fruit = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits:", list(add_my_fruit.index))
+
 
 
 
