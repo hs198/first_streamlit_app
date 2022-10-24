d0826e3037f07cd2eb54b5fc4a5e4ebd15393139
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-24 17:18:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 
 
 # Don't run anything past here while we troubleshoot
-streamlit.stop
+#streamlit.stop
 
 
 #import snowflake.connector
@@ -91,17 +91,37 @@ streamlit.stop
  
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
+#def get_fruit_load_list():
+  #with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
+    #return my_cur.fetchall()
   
   # Add a button to load the fruit
-  if streamlit.button('Get fruit load list'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
-                                                             
+ # if streamlit.button('Get fruit load list'):
+   # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   # my_data_rows = get_fruit_load_list()
+    #streamlit.dataframe(my_data_rows)
+ 
+#  Add a Second Text Entry Box: 
+#  Allow the end user to add a fruit to  the list:
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         # my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")   
+         return "Thanks for adding " + new_fruit
+        
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     back_from_function = insert_row_snowflake(add_my_fruit)
+     my_cnx.close()
+     streamlit.text(back_from_function)
+
+
+
+
+
+
   
 #Allow the end user to add a fruit to the list
 
