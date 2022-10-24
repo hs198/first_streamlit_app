74b481f5bad1bae7d7759949cda22ae3d19342b1
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-24 09:17:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,10 @@ streamlit.dataframe(my_data_row)
   
 Allow the end user to add a fruit to the list
 
+import pandas
 add_my_fruit = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-add_my_fruit = add_my_fruit.set_index('fruit')
+streamlit.dataframe(add_my_fruit)
+
 
 
 
