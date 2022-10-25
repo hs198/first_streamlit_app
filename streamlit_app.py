e5e2f6e19670c80eeae050c5985775775b976880
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-25 15:57:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,10 +91,18 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
    #   back_from_function = get_fruityvice_data(fruit_choice)
     #  streamlit.dataframe(back_from_function)
       
-#except URLError as e:
- #   streamlit.error()
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
-
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
 
 
 #import requests
