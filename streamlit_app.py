84b4923f97dc0c26f2dbdbb6bc373209d9c1d1c4
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-25 16:00:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work correctly, but go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 # Display the table on the page.
 #mport requests
@@ -92,17 +92,17 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     #  streamlit.dataframe(back_from_function)
       
 #Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
-        return my_cur.fetchall()
+#def get_fruit_load_list():
+    #with my_cnx.cursor() as my_cur:
+      #  my_cur.execute("select * from fruit_load_list")
+        #return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    my_cnx.close()
-    streamlit.dataframe(my_data_rows)
+#if streamlit.button('Get Fruit Load List'):
+    #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    #my_data_rows = get_fruit_load_list()
+    #my_cnx.close()
+    #streamlit.dataframe(my_data_rows)
 
 
 #import requests
