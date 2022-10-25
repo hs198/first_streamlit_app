c02464cea0808dc7c55fb6c4924ffde50014c159
hsidd <116047341+hs198@users.noreply.github.com>
2022-10-25 09:28:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,7 @@ streamlit.dataframe(my_data_rows)
 
 
 Import requests
-add_my_fruit = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered ', fruit_choice)
+add_my_fruit = request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 
 
